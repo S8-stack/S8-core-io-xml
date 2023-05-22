c598bbfa9017cc1f5736180f8596ef15493ced3c
Pierre Convert <pierreconvert@macbook-pro-6.home>
2023-05-22 21:59:57 +0200

    Update done

--- tests/com/s8/io/xml/tests/UnitTest04.java
+++ tests/com/s8/io/xml/tests/UnitTest04.java
@@ -24,7 +24,7 @@ public class UnitTest04 {
 	}
 
 	public static void generateDTD() throws XML_TypeCompilationException, IOException {
-		XML_Codebase context = new XML_Codebase(new Class<?>[] { MyTrain.class });
+		XML_Codebase context = XML_Codebase.from( MyTrain.class );
 		Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File("data/xml/input/train.dtd"))));
 		context.xsd_writeSchema(writer);
 		writer.close();
@@ -33,7 +33,7 @@ public class UnitTest04 {
 	public static void read() throws Exception {
 
 		Reader reader = new BufferedReader(new InputStreamReader(new FileInputStream(new File("data/xml/input/test-train.xml"))));
-		XML_Codebase context = new XML_Codebase(new Class<?>[] { MyTrain.class });
+		XML_Codebase context = XML_Codebase.from( MyTrain.class );
 
 		context.setVerbosity(true);
 		Object object = context.deserialize(reader);
